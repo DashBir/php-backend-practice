8a4081cb40d1da2571938bfa924d3a39970d52b3
DariiaBiriuchenko <ma4_bdya@student.ztu.edu.ua>
2026-08-28 15:06:11 +0300

    Create registrationForm.php

---
+++ lab05/task01/views/registrationForm.php
@@ -0,0 +1,18 @@
+<form method="post" action="index.php?page=register">
+    <h2>Register</h2>
+
+    <label for="login">Username:</label>
+    <input type="text" id="login" name="login" placeholder="Enter your username" required>
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" placeholder="Enter your password" required>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" placeholder="Enter your email" required>
+
+    <input type="submit" value="Register">
+</form>
+
+<p>
+    <a href="index.php">Already have an account? Log in</a>
+</p>
