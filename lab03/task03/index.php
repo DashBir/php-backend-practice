339d0d8c363969e58292d80c30c412caa5700dd2
DariiaBiriuchenko <ma4_bdya@student.ztu.edu.ua>
2026-08-28 11:17:23 +0300

    Create index.php

---
+++ lab03/task03/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<a href="task03a.php">Task03a</a><br>
+<a href="task03b.php">Task03b</a><br>
+<a href="task03c.php">Task03c</a><br>
+</body>
+</html>
